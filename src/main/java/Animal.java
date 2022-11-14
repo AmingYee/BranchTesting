ee0ce60fe65a73f48456f924035080f65602b11f
AmingYee <wenm0007@stud.kea.dk>
2022-11-14 13:03:24 +0100

    makesound commit

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -1,10 +1,10 @@
-public class Animal {
+public abstract class Animal {
     protected String name;
 
     public Animal(String name) {
         this.name = name;
     }
-
+    public abstract void makeSound();
     public String Name() {
         return name;
     }
